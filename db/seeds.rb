dd9db86cf51c79142a4fd9399afc9b5b40458d7d
kidusyohans <kidusyohans@hotmail.com>
2015-09-17 02:57:28 +0000

    Seeding-Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,29 @@
+require 'faker'
+ 
+## Create Users
+5.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save
+end
+ users = User.all
+ 
+## Create wikis
+20.times do 
+  wiki = Wiki.create!(
+    user: users.sample,
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph
+  )
+end
+
+wikis = Wiki.all
+puts "#{wikis.count} wikis created."
+
 # # Create an admin user
  admin = User.new(
    name:     'Admin User',
@@ -26,3 +52,5 @@
  )
  standard.skip_confirmation!
  standard.save!
+ 
+ 
